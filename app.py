b1f00b66a796a74fca1bd8c9d1aa2e65caec61d3
M SHARJEEL JAVAID <126756480+Shurjeel1@users.noreply.github.com>
2023-06-02 03:49:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,103 +11,63 @@ HTML_BANNER = """
     </div>
     """
 
-
 def main():
 	"""Basics on st.beta columns/layout"""
 	
 	menu = ["Home","Search","About"]
+
 	choice = st.sidebar.selectbox("Menu",menu)
-	stc.html(HTML_BANNER)
-
-	df = pd.read_csv("final_movielens_500_db.csv")
-	# Change Year to Datetime
-	df['year'] = pd.to_datetime(df['year'])
-
 
 	if choice == 'Home':
 		st.subheader("Home")
+		st.success("Full Layout")
+		# st.text("Sidebar")
+		# st.write(dir(st.sidebar))
+
+		# Using st.beta_columns
+		col1,col2 = st.beta_columns(2)
+		# st.text("Beta Columns")
+		# st.write(dir(col1))
+
+		# Method 1
+		col1.success("First Column")
+		col1.button("Hello")
 
 
-		# with st.beta_expander("Title"):
-		# 	mytext = st.text_area("Type Here")
-		# 	st.write(mytext)
-		# 	st.success("Hello")
+		col2.success("Second COlumn")
+		col2.button("Hello From Col2")
+
+		# Method 2: Context Manager
+		with col1:
+			search = st.text_area("Enter Text Here")
+			if st.button("Submit"):
+				st.write(search.upper())
 
 
-		# st.dataframe(df)
-		movies_title_list = df['title'].tolist()
-
-		movie_choice = st.selectbox("Movie Title",movies_title_list)
-		with st.beta_expander('Movies DF',expanded=False):
-			st.dataframe(df.head(10))
-
-			# Filter
-			img_link = df[df['title'] == movie_choice]['img_link'].values[0]
-			title = df[df['title']== movie_choice]['title'].values
-			genre = df[df['title']== movie_choice]['genres'].values
-
-
-		# Layout
-			# st.write(img_link)
-			# st.image(img_link)
-		c1,c2,c3 = st.beta_columns([1,2,1])
-
-		with c1:
-			with st.beta_expander("Title"):
-				st.success(title)
-
-
-		with c2:
-			with st.beta_expander("Image"):
-				st.image(img_link,use_column_with=True)
-
-
-		with c3:
-			with st.beta_expander("Genre"):
-				st.write(genre)
+		with col2:
+			year = st.number_input('Year',1995,2020)
 
 
 
 
 
 	elif choice == "Search":
-		st.subheader("Search Movies")
+		st.subheader("Search")
+		# Different Layout
+		c1,c2,c3 = st.beta_columns([3,1,1])
 
-		with st.beta_expander("Search By Year"):
-			movie_year = st.number_input("Year",1995,2020)
+		with c1:
+			st.info("From Col 1")
 
-			df_for_year = df[df['year'].dt.year == movie_year]
-			st.dataframe(df_for_year)
+		with c2:
+			st.success("From C2 :Less")
 
-		col1,col2,col3  = st.beta_columns([1,2,1])
+		with c3:
+			st.warning("From C3 :Less")
 
-		with col1:
-			with st.beta_expander("Title"):
-				for t in df_for_year['title'].tolist():
-					st.success(t)
-
-
-		with col2:
-			with st.beta_expander("Images"):
-				for i in df_for_year['img_link'].tolist():
-					st.image(i,use_column_with=True)
-
-
-		with col3:
-			with st.beta_expander("Genre"):
-				for g in df_for_year['genres'].tolist():
-					st.write(g)
-
-
-
-
-
-		
 
 	else:
 		st.subheader("About")
-		st.text("Built with Streamlit")
-		st.text("Jesus Saves @JCharisTech")
 
 
 
